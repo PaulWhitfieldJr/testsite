3a3720c3a70590a3e20d00c4b439b77bdb217944
PaulWhitfieldJr <113262572+PaulWhitfieldJr@users.noreply.github.com>
2022-09-14 13:50:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     document.getElementById("demo").innerHTML = this.responseText;
+     document.getElementById("home").innerHTML = this.responseText;
     }
   };
-  xhttp.open("GET", "ajax_info.txt", true);
+  xhttp.open("GET", "hello.txt", true);
   xhttp.send();
 }
